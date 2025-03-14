2e71913c926722829cad3a2a2f668d9e5bf8ec5a
Adelin Rosca <rosca.adelin@gmail.com>
2025-03-14 13:25:11 +0200

    added the two fonts that will be used in app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -9,7 +10,20 @@ export const metadata: Metadata = {
   description:
     "A modern crypto platform for tracking, comparing, and managing digital assets.",
   // TODO: add openGraph and twitter entries, when the app will be deployed
+  // TODO: add a sitemap when the app will be deployed
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+const space_grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-space-grotesk",
+});
 
 export default function RootLayout({
   children,
@@ -17,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${space_grotesk.variable}`}>
       <body className={`antialiased`}>{children}</body>
     </html>
   );
