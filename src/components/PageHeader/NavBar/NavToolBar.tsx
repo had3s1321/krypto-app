57868ddb19f39749cda8b40ff4762dd5bfb3538e
Adelin Rosca <rosca.adelin@gmail.com>
2025-03-15 22:59:33 +0200

    added theme toggle functionality

--- src/components/PageHeader/NavBar/NavToolBar.tsx
+++ src/components/PageHeader/NavBar/NavToolBar.tsx
@@ -1,11 +1,13 @@
-import CurrencyDropdown from "./CurrencyDropdow";
+import CurrencyDropdown from "./CurrencyDropdown";
 import SearchBar from "./SearchBar";
+import ThemeToggler from "./ThemeToggler";
 
 const NavToolBar = () => {
   return (
     <div className="flex gap-3 pt-1">
       <SearchBar />
       <CurrencyDropdown />
+      <ThemeToggler />
     </div>
   );
 };
