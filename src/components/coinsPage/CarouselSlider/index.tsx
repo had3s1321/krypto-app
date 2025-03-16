e8227f0275891f20e37c997b4985fb44f2be9ac9
Adelin Rosca <rosca.adelin@gmail.com>
2025-03-16 22:57:59 +0200

    finished the carousel ui

--- src/components/coinsPage/CarouselSlider/index.tsx
+++ src/components/coinsPage/CarouselSlider/index.tsx
@@ -1,32 +1,61 @@
 "use client";
 
+import { useState } from "react";
+import Image from "next/image";
 import { DownIcon, UpIcon } from "@/components/ui/icons";
 import { Card, CardContent } from "@/components/ui/shadcn/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from "@/components/ui/shadcn/carousel";
 import { coins_market_data } from "@/utils/mockData/coins_market_data";
 import { parseCarouselSliderData } from "@/utils/parseCarouselSliderData";
-import Image from "next/image";
+
+interface CarouselItemInterface {
+  name: string;
+  symbol: string;
+  image: string;
+  price: number;
+  priceChange: number;
+}
 
 const CarouselSlider = () => {
+  const [selectedItems, setSelectedItems] = useState<CarouselItemInterface[]>(
+    [],
+  );
   const data = parseCarouselSliderData(coins_market_data);
+  const isSelectedClass =
+    "text-[var(--clr-light-perm)] bg-[var(--primary-foreground)]";
+
+  const handleClick = (item: CarouselItemInterface) => {
+    if (selectedItems.some((el) => item.name === el.name)) {
+      const newItems = selectedItems.filter((el) => item.name !== el.name);
+      setSelectedItems(newItems);
+      return;
+    }
+    if (selectedItems.length > 1) setSelectedItems([selectedItems[1], item]);
+    else setSelectedItems([...selectedItems, item]);
+  };
 
   return (
     <>
       <h3 className="mb-8 font-grotesk text-sm text-[var(--clr-nav-text)]">
         Select the currency to view statistics
       </h3>
-      <Carousel className="w-full">
+      <Carousel className="mb-8 w-full">
         <CarouselContent className="-ml-4">
-          {data.slice(0, 5).map((el) => (
+          {data.map((el) => (
             <CarouselItem
               key={el.name}
+              onClick={() => handleClick(el)}
               className="basis-1/5 [&:not(:first-child)]:pl-2"
             >
-              <div className="rounded-md bg-[var(--foreground)]">
+              <div
+                className={`rounded-md ${selectedItems.some((item) => item.name === el.name) && isSelectedClass} bg-[var(--foreground)]`}
+              >
                 <Card className="flex items-center border-none px-4 shadow-none">
                   <Image src={el.image} alt={el.name} width={32} height={32} />
                   <CardContent className="flex flex-col items-start justify-center gap-1 p-3 text-[var(--clr-text)]">
@@ -48,6 +77,8 @@ const CarouselSlider = () => {
             </CarouselItem>
           ))}
         </CarouselContent>
+        <CarouselPrevious className="top-10 ml-6 bg-[var(--primary-foreground)] hover:cursor-pointer" />
+        <CarouselNext className="top-10 mr-6 bg-[var(--primary-foreground)] hover:cursor-pointer" />
       </Carousel>
     </>
   );
