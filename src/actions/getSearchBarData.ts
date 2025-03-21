dccb306de33d4285f4b14706e7dd5751d648510d
Adelin Rosca <rosca.adelin@gmail.com>
2025-03-21 11:43:25 +0200

    added debounce functionality to the search component

--- src/actions/getSearchBarData.ts
+++ src/actions/getSearchBarData.ts
@@ -1,9 +1,13 @@
 "use server";
 
 import { fetchData } from "@/utils/fetchData";
+import { search_bar } from "@/utils/mockData/search_bar";
 import { SearchBarData } from "@/utils/types/SearchBarData";
 
 export const getSearchBarData = async (query: string) => {
-  const response = await fetchData<SearchBarData>(`search?query=${query}`);
+  const response: SearchBarData = await fetchData(
+    `search?query=${query}`,
+    search_bar,
+  );
   return response;
 };
