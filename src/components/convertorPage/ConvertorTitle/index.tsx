4ccc01ab7501e3d715a58caaefbcca6e0a838e10
Adelin Rosca <rosca.adelin@gmail.com>
2025-03-30 15:49:50 +0300

    updated the page title to display current date and time

--- src/components/convertorPage/ConvertorTitle/index.tsx
+++ src/components/convertorPage/ConvertorTitle/index.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useAppSelector } from "@/lib/hooks";
+import { getDate, getTime } from "@/utils/formatUtils";
+
 const ConvertorTitle = () => {
-  // TODO add dynamic date and time
+  const { locale } = useAppSelector((state) => state.user);
+  const date = getDate("short", locale);
+  const time = getTime(locale);
+
   return (
     <div className="mb-6 font-grotesk text-[var(--clr-nav-text)]">
       <h3 className="mb-1 text-xl font-medium">Online currency convertor</h3>
-      <span>04/25/2025 14:15</span>
+      <span>
+        {date} {time}
+      </span>
     </div>
   );
 };
