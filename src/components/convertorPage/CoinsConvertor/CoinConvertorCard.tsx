176dbf105d64d9cb59ff5a57c6320a41e41836a5
Adelin Rosca <rosca.adelin@gmail.com>
2025-03-30 16:15:14 +0300

    selected coins are new default comparison coins

--- src/components/convertorPage/CoinsConvertor/CoinConvertorCard.tsx
+++ src/components/convertorPage/CoinsConvertor/CoinConvertorCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { CarouselItemInterface } from "@/components/coinsPage/CoinSlider/CoinsCarousel";
 import {
   Card,
   CardContent,
@@ -5,23 +8,50 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/shadcn/card";
+import { useFormat } from "@/hooks/useFormat";
 import Image from "next/image";
-// eslint-disable-next-line
-const CoinConvertorCard = ({ title, coin }: { title: string; coin: any }) => {
+
+const CoinConvertorCard = ({
+  title,
+  coin,
+}: {
+  title: string;
+  coin?: CarouselItemInterface;
+}) => {
+  const format = useFormat();
+
   return (
-    <Card className="w-full border-none bg-white text-black shadow-none">
+    <Card className="w-full border-none bg-[var(--foreground)] text-[var(--clr-nav-text)] shadow-none">
       <CardHeader>
-        <CardTitle className="text-sm font-normal">{title}</CardTitle>
+        <CardTitle className="cursor-default text-sm font-normal">
+          {title}
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <span className="mb-4 flex gap-2 text-xl font-medium">
-          <Image src={coin.image} alt={coin.name} width={24} height={24} />
-          {coin.name} ({coin.symbol})
+          {coin && (
+            <Image src={coin.image} alt={coin.name} width={24} height={24} />
+          )}
+          {coin ? (
+            <>
+              {coin.name} ({coin.symbol})
+            </>
+          ) : (
+            "Please select a coin"
+          )}
         </span>
-        <div className="h-[1px] w-full bg-black"></div>
+        <div className="h-[1px] w-full bg-[var(--clr-text)]"></div>
       </CardContent>
-      <CardFooter className="-mt-4">
-        1 {coin.symbol} = {coin.price}
+      <CardFooter className="-mt-4 cursor-default">
+        {coin && (
+          <>
+            1 {coin.symbol} ={" "}
+            {format(coin.price, {
+              style: "currency",
+              maximumFractionDigits: 2,
+            })}
+          </>
+        )}
       </CardFooter>
     </Card>
   );
--- src/components/convertorPage/CoinsConvertor/index.tsx
+++ src/components/convertorPage/CoinsConvertor/index.tsx
@@ -1,24 +1,15 @@
+"use client";
+
+import { useAppSelector } from "@/lib/hooks";
 import CoinConvertorCard from "./CoinConvertorCard";
 
 const CoinsConvertor = () => {
-  const bitcoin = {
-    name: "Bitcoin",
-    image:
-      "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
-    symbol: "btc",
-    price: "82937",
-  };
-  const ethereum = {
-    name: "Ethereum",
-    image:
-      "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
-    symbol: "eth",
-    price: "1887.61",
-  };
+  const { selectedCoins } = useAppSelector((state) => state.user);
+
   return (
     <div className="mb-8 flex gap-6 font-grotesk">
-      <CoinConvertorCard title="You sell" coin={bitcoin} />
-      <CoinConvertorCard title="You buy" coin={ethereum} />
+      <CoinConvertorCard title="You sell" coin={selectedCoins[0]} />
+      <CoinConvertorCard title="You buy" coin={selectedCoins[1]} />
     </div>
   );
 };
