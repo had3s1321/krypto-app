7be2405f1fa854521acde9347e532058e729d4ed
Adelin Rosca <rosca.adelin@gmail.com>
2025-05-15 10:42:03 +0300

    quick-fix mobile nav bar link names are correctly displayed

--- src/components/PageHeader/NavBar/Mobile/MobileLink.tsx
+++ src/components/PageHeader/NavBar/Mobile/MobileLink.tsx
@@ -5,19 +5,20 @@ import { usePathname } from "next/navigation";
 
 interface MobileLinkProps {
   children: React.ReactNode;
+  name: "Overview" | "Convertor" | "Portfolio";
   path: "/" | "/convertor" | "/portfolio";
 }
 
-const MobileLink = ({ children, path }: MobileLinkProps) => {
+const MobileLink = ({ children, name, path }: MobileLinkProps) => {
   const pathname = usePathname();
 
   const onPath = path === pathname && "bg-[var(--primary-foreground)]";
 
   return (
-    <li className={`w-[30%] rounded-sm py-3 hover:cursor-pointer ${onPath}`}>
-      <Link href={path} className="flex flex-col items-center">
+    <li className={`w-[30%] rounded-md py-4 hover:cursor-pointer ${onPath}`}>
+      <Link href={path} className="flex flex-col items-center gap-1">
         {children}
-        <span className="text-xs">Overview</span>
+        <span className="text-[10px]">{name}</span>
       </Link>
     </li>
   );
--- src/components/coinsPage/CoinTable/NameCell.tsx
+++ src/components/coinsPage/CoinTable/NameCell.tsx
@@ -10,21 +10,23 @@ interface NameCellProps {
 
 const NameCell = ({ image, name, symbol }: NameCellProps) => {
   const breakpoint = useScreenBreakpoint();
+
+  const imageSize = breakpoint === "md" ? 24 : 28;
   const formattedSymbol =
     breakpoint === "md" ? symbol.toUpperCase() : `(${symbol.toUpperCase()})`;
 
   return (
     <TableCell className="w-[35%] rounded-l-md font-semibold text-[var(--clr-text)] md:w-[20%] md:rounded-l-none">
-      <div className="flex w-full items-center gap-4">
+      <div className="flex w-full items-center gap-2 md:gap-4">
         <Image
           src={image}
           alt={name}
-          width={28}
-          height={28}
+          width={imageSize}
+          height={imageSize}
           className="shrink-0"
         />
-        <div className="flex w-full flex-col-reverse items-start overflow-hidden text-ellipsis md:flex-row md:items-center">
-          <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+        <div className="flex w-full flex-col-reverse items-start gap-0 overflow-hidden text-ellipsis md:flex-row md:items-center md:gap-1">
+          <span className="hidden overflow-hidden text-ellipsis whitespace-nowrap md:block">
             {name}
           </span>
           <span className="overflow-hidden text-ellipsis whitespace-nowrap">
