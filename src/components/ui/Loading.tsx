e6d97af4ac11ec60a31ab1dc38adba53913ce82e
Adelin Rosca <rosca.adelin@gmail.com>
2025-06-29 15:07:45 +0300

    added loader for redux persist

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,11 @@
+import { FadeStaggerSquares } from "./icons";
+
+const Loading = () => (
+  <div className="flex h-[100vh] w-full items-center justify-center bg-[#13121b]">
+    <div className="h-8 w-8">
+      <FadeStaggerSquares />
+    </div>
+  </div>
+);
+
+export default Loading;
